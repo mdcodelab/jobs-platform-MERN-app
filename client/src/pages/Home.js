cd84e4b1255ffe70f08b73f8f51e6198cea1f39f
miha11-hash <d_mihaela@msn.com>
2024-01-05 13:52:28 +0200

    Home page style

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,45 +1,64 @@
 import React from "react";
-import logo from "../assets/images/logo.png";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import Logo from "../components/Logo";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function Home() {
+  React.useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <Wrapper>
       <nav>
-        <div className="nav-content">
-          <img src={logo} alt="work wave hub" className="logo"></img>
+        <div
+          className="nav-content"
+          data-aos="slide-right"
+          data-aos-duration="1500"
+        >
+          <h2 className="logo">W</h2>
           <h4>Work Wave Hub</h4>
         </div>
       </nav>
       <div className="container">
-        <video className="video" autoPlay muted playsInline loop>
+        <video className="video" autoPlay muted playsInline loop preload="auto">
           <source src="assets/video.mp4" type="video/mp4"></source>
         </video>
         <div className="cover">
-          <h1>
-            Job <span>tracking</span> platform
-          </h1>
-          <p>
-            This job platform is tailored to meet the demands of modern
-            professionals and businesses alike. Whether you're an individual
-            seeking your next career opportunity or an organization striving to
-            find the best talent, our platform bridges the gap, offering a
-            seamless, intuitive experience. Dive into a vast pool of
-            opportunities, connect with like-minded professionals, and usher in
-            a new era of work, all at your fingertips!
-          </p>
-          <Link className="btn btn-hero" to="/register">
-            Login/Register
-          </Link>
+          <div className="cover__content">
+            <h1 data-aos="slide-down" data-aos-duration="2500">
+              Job <span>Tracking</span> Platform
+            </h1>
+            <p data-aos="fade" data-aos-duration="1500">
+              This job platform is tailored to meet the demands of modern
+              professionals and businesses alike. Whether you're an individual
+              seeking your next career opportunity or an organization striving
+              to find the best talent, our platform bridges the gap, offering a
+              seamless, intuitive experience. Dive into a vast pool of
+              opportunities, connect with like-minded professionals, and usher
+              in a new era of work, all at your fingertips!
+            </p>
+            <Link
+              className="btn btn-hero"
+              to="/register"
+                
+              data-aos-duration="2000"
+            >
+              Login/Register
+            </Link>
+          </div>
         </div>
       </div>
       <div className="footer">
         <div className="copyright">
-          <p className="copyright__footer">
-            &copy; {new Date().getFullYear()} <span>W</span>
-            <span> Work Wave Hub</span>. All Rights Reserved.
+          <p className="footer__info">
+            <span className="date">&copy; {new Date().getFullYear()}</span>
+            <span className="logo__full">
+              <span className="logo__letter">W</span>
+              <span className="logo__name"> Work Wave Hub.</span>
+            </span>
+            All Rights Reserved.
           </p>
         </div>
       </div>
@@ -52,7 +71,7 @@ const Wrapper = styled.main`
   height: 100vh;
   margin: 0 auto;
   position: relative;
-  background: var(--primary-900);
+  overflow-y: hidden;
 
   nav {
     width: 100%;
@@ -62,14 +81,23 @@ const Wrapper = styled.main`
   .nav-content {
     display: flex;
     height: 100%;
-    width: 16rem;
+    width: 10rem;
     align-items: center;
     justify-content: space-between;
+    width: 14.5rem;
+    height: 100%;
   }
 
-  nav img {
-    width: 4rem;
-    height: 4rem;
+  .logo {
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+    background: var(--primary-500);
+    margin: 0;
   }
 
   nav h4 {
@@ -87,16 +115,68 @@ const Wrapper = styled.main`
     width: 100%;
     height: 100%;
     object-fit: cover;
-    border-radius: 1rem;
+    border-radius: 0.5rem;
+    margin: 0 auto;
   }
 
   .cover {
     position: absolute;
-    top: 5rem;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
     z-index: 10;
     height: calc(100vh - 11rem);
     width: 100%;
-    background: rgba(0,0,0,0.5);
+    background: rgba(0, 0, 0, 0.3);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .cover__content {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .cover__content h1 {
+    text-align: center;
+    margin: 0;
+    color: #fff;
+    font-size: 3rem;
+    text-shadow: 0 1px 3px white;
+    margin-bottom: 3rem;
+  }
+
+  .cover__content h1 span {
+    color: var(--primary-500);
+  }
+
+  .cover__content a {
+    display: block;
+    margin: 0 auto;
+    width: max-content;
+    margin-top: 2.5rem;
+    
+    animation: appear 2.5s ease;
+  }
+
+  @keyframes appear {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  .cover__content p {
+    color: #fff;
+    margin: 0;
+    padding: 0;
   }
 
   //
@@ -106,7 +186,53 @@ const Wrapper = styled.main`
     display: flex;
     align-items: center;
     justify-content: center;
-    color: var(--primary-500);
+    color: #fff;
+    padding-top: 2rem;
+  }
+
+  .footer__info {
+    width: 22rem;
+    height: max-content;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .logo__full {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 8.7rem;
+  }
+
+  .logo__letter {
+    width: 1.5rem;
+    height: 1.5rem;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: var(--primary-400);
+  }
+
+  .logo__name {
+    color: var(--primary-400);
+    font-size: 1rem;
+  }
+
+  @media (max-width: 362px) {
+    .footer {
+      padding-top: 1rem;
+    }
+    .footer__info {
+      flex-direction: column;
+      height: max-content;
+      line-height: 1.2rem;
+    }
+    .footer__info span {
+      margin: 0;
+      padding: 0;
+    }
   }
 `;
 
